69d47c240ebcb745bd92e79215a1fee55d7af59b
yukiharasaki <pongsaton.san@mail.pbru.ac.th>
2020-09-28 10:36:53 +0700

    index

--- pages/productinsert.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connect/cn.php");
-$tempname=$_POST['productname'];
-$tempnum=$_POST['productnum'];
-$temptypeid=$_POST['typeid'];
-$sql="INSERT INTO tbproduct(proname,pronum,typeid) values('".$tempname."','".$tempnum."','".$temptypeid."')";
-$result = mysqli_query($mysqli_cn, $sql);
-echo "<script>window.location='mainproduct.php'</script>";
-?>
