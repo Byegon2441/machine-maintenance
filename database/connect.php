2137877d805a9567f2c10c6d87b10a46ab2f2ee2
Patthapong Niamkerd <aeknicotine@gmail.com>
2020-09-30 14:55:24 +0700

    connect db

--- database/connect.php
+++ database/connect.php
@@ -3,8 +3,8 @@ $host ='localhost';
 $user ='root';
 $pass="";
 $database="cars";
-$connect = mysqli_connect($host,$user,$pass,$database);
-mysqli_set_charset($connect,"utf8");
+$connect = mysqli_connect("localhost","root","","cars");
+mysqli_query($connect,"SET NAMES UTF8");
 
 if(!$connect){
     die ("Connect Fail : ".mysqli_connect_error());
