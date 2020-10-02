d7ec67898804150ec8a49ec321f48fabd6aedd1c
yukiharasaki <pongsaton.san@mail.pbru.ac.th>
2020-10-02 12:06:54 +0700

    Update form

--- database/updateTypeMc.php
+++ database/updateTypeMc.php
@@ -1,7 +1,8 @@
 <?php
     include 'connect.php';
     $name = $_POST['name'];
-    $query = "INSERT INTO tmstmvehicletype(XVVehTypeName) VALUES ('".$name."');";
+    $id = $_POST['id'];
+    $query = "UPDATE tmstmvehicletype SET XVVehTypeName = '".$name."' WHERE XVVehTypeCode= '".$id."' ;";
     $sql = mysqli_query($connect,$query);
     mysqli_close($connect);
     echo "<script>window.location='../pages/ListTypeMachine.php'</script>";
