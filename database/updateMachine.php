f31667494446406d8d6b048a034b633e9e615f47
barbiekk <69235339+barbiekk@users.noreply.github.com>
2020-10-03 13:16:43 +0700

    Create updateMachine.php

---
+++ database/updateMachine.php
@@ -0,0 +1,60 @@
+<?php
+include 'connect.php';
+if ( isset( $_POST['updatedata'] ) ) {
+    $id = $_POST['update_id'];
+
+
+    $XVVehName = $_POST['XVVehName'];
+    $XVVehRegistration = $_POST['XVVehRegistration'];
+    $XVVehNumber = $_POST['XVVehNumber'];
+    $XVVehMango = $_POST['XVVehMango'];
+    $XVVehBrand = $_POST['XVVehBrand'];
+    $XVVehModel = $_POST['XVVehModel'];
+    $XVVehChassisNumber = $_POST['XVVehChassisNumber'];
+    $XVVehEngineNumber = $_POST['XVVehEngineNumber'];
+    $XVVehTypeCode = $_POST['XVVehTypeName'];
+    $sql = "UPDATE tmstvehicle
+     SET XVVehName='$name',
+     XVVehName ='$XVVehName',
+     XVVehRegistration ='$XVVehRegistration',
+     XVVehNumber ='$XVVehNumber',
+     XVVehMango ='$XVVehMango',
+     XVVehBrand ='$XVVehBrand',
+     XVVehModel ='$XVVehModel',
+     XVVehChassisNumber ='$XVVehChassisNumber',
+     XVVehEngineNumber ='$XVVehEngineNumber',
+     XVVehTypeCode ='$XVVehTypeCode';
+    
+    
+    
+     WHERE XVVehCode = '$id',
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     ";
+    $result = mysqli_query( $connect, $sql );
+
+    if ( $result ) {
+        echo '<script>';
+        echo "alert('ทำการแก้ไขชื่อเครื่องจักรเรียบร้อย');";
+        echo "window.location='../pages/ListMachine.php';";
+        echo '</script>';
+    } else {
+        echo '<script>';
+        echo "alert('ไม่สามารถทำการแก้ไขชื่อเครื่องจักรได้!!!');";
+        echo "window.location='../pages/ListMachine.php';";
+        echo '</script>';
+    }
+}
+
+?>
+
+
+
+
