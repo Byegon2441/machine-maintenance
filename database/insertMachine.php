72efb2581ceb1ce8bc7924a4448f762167eb45fd
barbiekk <69235339+barbiekk@users.noreply.github.com>
2020-10-03 13:16:40 +0700

    Update insertMachine.php

--- database/insertMachine.php
+++ database/insertMachine.php
@@ -11,12 +11,7 @@
     $XVVehEngineNumber = $_POST['XVVehEngineNumber'];
     $XVVehTypeCode = $_POST['XVVehTypeName'];
     $query = "INSERT INTO tmstvehicle(XVVehName,XVVehRegistration,XVVehNumber,XVVehMango,XVVehBrand,XVVehModel,XVVehChassisNumber,XVVehEngineNumber, XVVehTypeCode)
-     VALUES ('$XVVehName','$XVVehRegistration','$XVVehNumber','$XVVehMango','$XVVehBrand','$XVVehModel','$XVVehChassisNumber','$XVVehEngineNumber','$XVVehTypeCode');";
-     
-    //  $query = "UPDATE tmstvehicle SET XVVehCode = '".$XVVehCode."' WHERE XVVehCode= '".$XVVehCode."' ;";
-    
-    
-    
+     VALUES ('$XVVehName','$XVVehRegistration','$XVVehNumber','$XVVehMango','$XVVehBrand','$XVVehModel','$XVVehChassisNumber','$XVVehEngineNumber','$XVVehTypeCode');";  
 
     if(mysqli_query($connect,$query)){
         echo "YeaH";
@@ -26,3 +21,6 @@
     mysqli_close($connect);
      echo "<script>window.location='../pages/ListMachine.php'</script>";
 ?>
+
+
+
