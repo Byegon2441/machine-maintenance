b5a4c9c322f7c4b516c58e5b9fd70d31d905cb62
yukiharasaki <pongsaton.san@mail.pbru.ac.th>
2020-10-05 13:54:14 +0700

    Update+Insert EMP

---
+++ database/updateEmp.php
@@ -0,0 +1,31 @@
+<?php
+include '../database/connect.php';
+if ( isset( $_POST['updatedata'] ) ) {
+    $XVEpyCode = $_POST['XVEpyCode'];
+    $XVEpyFirstname = $_POST['XVEpyFirstname'];
+    $XVpyLastname = $_POST['XVpyLastname'];
+    $XVIdCardNumber = $_POST['XVIdCardNumber'];
+    $XVEpyJobPosition = $_POST['XVEpyJobPosition'];
+
+  $sql = "UPDATE tmstmtemployee
+     SET  XVEpyFirstname ='$XVEpyFirstname',
+     XVpyLastname ='$XVpyLastname',
+     XVIdCardNumber ='$XVIdCardNumber',
+     XVEpyJobPosition ='$XVEpyJobPosition'
+     WHERE XVEpyCode = $XVEpyCode;
+     ";
+    $result = mysqli_query($connect,$sql);
+   if ( $result ) {
+        echo '<script>';
+        echo "alert('ทำการแก้ไขข้อมูลพนักงานได้');";
+        echo "window.location='../pages/ListEmployee.php';";
+        echo '</script>';
+    } else {
+        echo '<script>';
+        echo "alert('ไม่สามารถทำการแก้ไขข้อมูลพนักงานได้');".echo mysqli_error($connect);
+        echo "window.location='../pages/ListEmployee.php';";
+        echo '</script>';
+    }
+}
+mysqli_close($connect);
+?>
