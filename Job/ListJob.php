7124aa297fb9d5df862c3687eb4ecc3b9bafa137
Patthapong Niamkerd <aeknicotine@gmail.com>
2020-10-16 19:40:42 +0700

    ดึงข้อมูลมาแสดงตอนแก้ไข

--- Job/ListJob.php
+++ Job/ListJob.php
@@ -203,7 +203,7 @@
     <!-- modal insert -->
 
 
-<!-- modal insert -->
+<!-- modal update -->
 <div class="modal fade bd-example-modal-lg1" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-lg">
@@ -216,12 +216,24 @@
                 </div>
                 <div class="modal-body">
                     <label>ใบแจ้งซ่อม</label>
-                    <form action="insertMajor.php" class="form-inline" method="POST">
+                    <form action="updateMajor.php" class="form-inline" method="POST">
+                        
+                        
+                    <div class="row">
+                            <div class="col-md-6"></div>
+                            <div class="col-md-6 ml-auto">
+                                <div class="col text-right">
+                                    <label for="numb">เลขที่ใบแจ้งซ่อม : <input type="text" name="numb" id="jobid"
+                                            class="form-control" readonly></label>
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="row">
                             <div class="col-md-6"></div>
                             <div class="col-md-6 ml-auto">
                                 <div class="col text-right">
-                                    <label for="numb">วันที่แจ้งซ่อม : <input type="text" name="numb"
+                                    <label for="numb">วันที่แจ้งซ่อม : <input type="text" name="numb" id="jobdate"
                                             class="form-control" data-toggle="datepicker" readonly></label>
                                 </div>
                             </div>
@@ -231,7 +243,7 @@
                             <div class="col-md-7">
                                 <div class="col text-right">
                                     <label for="numb">ชื่อเครื่องจักร :</label>
-                                    <select id="XVVehName" name="XVVehName" class="form-control" style="width:60%">
+                                    <select id="vehcode" name="XVVehName" class="form-control" style="width:60%">
                                         <?php
                                      include '../database/connect.php';
                                      $sql = "select * from tmstvehicle; ";
@@ -249,7 +261,7 @@
                             </div>
                             <div class="col-md-5">
                                 <div class="col text-right">
-                                    <label for="numb">หมายเลขเครื่องจักร : <input type="text" size="17" name="noof"
+                                    <label for="numb">หมายเลขเครื่องจักร : <input type="text" size="17" name="noof"id="vehcode2"
                                             class="form-control" id="noof" readonly></label>
                                 </div>
                             </div>
@@ -258,14 +270,14 @@
                         <div class="row">
                             <div class="col-md-7">
                                 <div class="col text-right">
-                                    <label for="numb">ชื่อไชต์งาน : <input type="text" size="30" name="dname" id="dname"
+                                    <label for="numb">ชื่อไชต์งาน : <input type="text" size="30" name="dname" id="department_name"
                                             class="form-control" readonly></label>
                                 </div>
                             </div>
                             <div class="col-md-5">
                                 <div class="col text-right">
                                     <label for="numb">หมายเลขหน่วยงาน : <input type="text" size="17" name="dcode"
-                                            id="dcode" class="form-control" readonly></label>
+                                    id="department_id" class="form-control" readonly></label>
                                 </div>
                             </div>
                         </div>
@@ -273,13 +285,13 @@
                         <div class="form-row">
                             <div class="col">
                                 <label for="numb">ตำแหน่งเครื่องจักร ณ ปัจจุบัน เลขที่ :
-                                    <input type="text" style="margin: 0px 10px;" size="10" name="dnum" id="dnum"
+                                    <input type="text" style="margin: 0px 10px;" size="10" name="dnum" id="number"
                                         class="form-control">
-                                    <input type="text" style="margin: 0px 10px;" size="10" name="dsub" id="dsub"
+                                    <input type="text" style="margin: 0px 10px;" size="10" name="dsub" id="sub"
                                         class="form-control">
-                                    <input type="text" style="margin: 0px 10px;" size="10" name="ddis" id="ddis"
+                                    <input type="text" style="margin: 0px 10px;" size="10" name="ddis" id="dis"
                                         class="form-control">
-                                    <input type="text" style="margin: 0px 10px;" size="10" name="dpro" id="dpro"
+                                    <input type="text" style="margin: 0px 10px;" size="10" name="dpro" id="pro"
                                         class="form-control"></label>
                             </div>
                         </div>
@@ -350,7 +362,7 @@
             </div>
         </div>
     </div>
-    <!-- modal insert -->
+    <!-- modal update -->
 
     <div id="wrapper">
         <div id="page-wrapper">
@@ -382,19 +394,26 @@
                                             <th>เลขที่ใบแจ้งซ่อม</th>
                                             <th>วันที่แจ้ง</th>
                                             <th>หมายเลขเครื่องจักร</th>
-                                            <th>ชื่องานหรือชื่อเครื่องจักร</th>
+                                            <th>ชื่อเครื่องจักร</th>
                                             <th>สถานะใบแจ้งซ่อม</th>
                                             <th>สถานะเอกสาร</th>
+                                            <th style="display:none;">ชื่อไซต์งาน</th>
+                                            <th style="display:none;">รหัสไซต์งาน</th>
+                                            <th style="display:none;">เลขที่</th>
+                                            <th style="display:none;">ตำบล</th>
+                                            <th style="display:none;">อำเภอ</th>
+                                            <th style="display:none;">จังหวัด</th>
                                             <th>รายละเอียด</th>
                                             <th>จัดการ</th>
                                         </tr>
                                     </thead>
                                     <?php
           include '../database/connect.php';
-          $sql = " SELECT m.XVMajDocNo, d.XDMajDate, m.XVVehCode, v.XVVehName, m.XVMajStatus, m.XVMajDocStatus
-          FROM  tdoctmajob m, tdoctmajobdate d, tmstvehicle v
+          $sql = " SELECT m.XVMajDocNo, d.XDMajDate, m.XVVehCode, v.XVVehName, m.XVMajStatus, m.XVMajDocStatus ,depart.XVDptCode,depart.XVDptName,depart.XVDptNumber,depart.`XVDptSub-district`,depart.XVDptDistrict,depart.XVDptProvince
+          FROM  tdoctmajob m, tdoctmajobdate d, tmstvehicle v,tmstmdepartment depart 
           WHERE m.XVMajDocNo = d.XVMajDocNo 
-          AND m.XVVehCode = v.XVVehCode"; //ตัวสมบูรณ์
+          AND m.XVVehCode = v.XVVehCode
+          AND v.XVDptCode = depart.XVDptCode;"; //ตัวสมบูรณ์
         //   $sql = " SELECT m.XVMajDocNo, m.XVVehCode, v.XVVehName, m.XVMajStatus FROM tdoctmajob m, tmstvehicle v WHERE m.XVVehCode = v.XVVehCode ";
         //   ตัวทดสอบ
          
@@ -405,7 +424,7 @@
           ?>
 
                                     <tr class="odd gradeA">
-                                        <td><?php echo $row["XVMajDocNo"];?></td>
+                                    <td><?php echo $row["XVMajDocNo"];?></td>
                                         <td><?php echo $row["XDMajDate"];?></td>
                                         <td><?php echo $row["XVVehCode"];?></td>
                                         <td><?php echo $row["XVVehName"];?></td>
@@ -417,10 +436,16 @@
                                             }else{
                                                 echo 'ยกเลิก'; 
                                             } ?></td>
-                                        <td align="center"><button class='btn btn-primary editbtn'
-                                                type='button'>รายละเอียด</button></td>
-                                        <td align="center"><button class='btn btn-danger deletebtn'
-                                                type='button'>ยกเลิก</button></td>
+                                            <td style="display:none;"><?php echo $row["XVDptName"];?></td>
+                                            <td style="display:none;"><?php echo $row["XVDptCode"];?></td>
+                                            <td style="display:none;"><?php echo $row["XVDptNumber"];?></td>
+                                            <td style="display:none;"><?php echo $row["XVDptSub-district"];?></td>
+                                            <td style="display:none;"><?php echo $row["XVDptDistrict"];?></td>
+                                            <td style="display:none;"><?php echo $row["XVDptProvince"];?></td>
+                                        <td align="center"><button class='btn btn-primary editbtn' type='button'
+                                                >รายละเอียด</button></td>
+                                        <td align="center"><button class='btn btn-danger deletebtn' type='button'
+                                                >ยกเลิก</button></td>
 
                                     </tr>
                                     <?php $count++;}
@@ -522,6 +547,34 @@
                 alert("Please Select Machine");
             }
         })
+
+        $('#vehcode').change(() => {
+            var id = $('#vehcode').val();
+            if (id != '') {
+                console.log(id)
+                $.ajax({
+                    url: "fetch.php",
+                    method: "POST",
+                    data: {
+                        id: id
+                    },
+                    dataType: "JSON",
+                    success: function(data) {
+                        $('#vehcode2').val(data.numb);
+                        $('#department_id').val(data.dcode);
+                        $('#department_name').val(data.dname);
+                        $('#number').val(data.dnum);
+                        $('#sub').val(data.dsub);
+                        $('#dis').val(data.ddis);
+                        $('#pro').val(data.dpro);
+                    }
+                })
+            } else {
+                alert("Please Select Machine");
+            }
+        })
+
+
         $(document).ready(function() {
             $('.editbtn').on('click', function() {
                 $('.bd-example-modal-lg1').modal('show')
@@ -532,8 +585,17 @@
                 }).get()
 
                 console.log(data)
-                // $('#update_id').val(data[1])
-                // $('#XVVehTypeName').val(data[2])
+            $('#jobid').val(data[0])
+            $('#jobdate').val(data[1])   
+            $('#vehcode').val(data[2])
+            $('#vehcode2').val(data[2]) 
+            $('#department_name').val(data[6]) //6 รายละเอียด 7 ยกเลิก
+            $('#department_id').val(data[7]) 
+            $('#number').val(data[8]) 
+            $('#sub').val(data[9]) 
+            $('#dis').val(data[10]) 
+            $('#pro').val(data[11]) 
+
             })
         })
         </script>
