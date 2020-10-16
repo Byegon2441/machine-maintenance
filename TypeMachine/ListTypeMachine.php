b159cf3d092f5efb5aad11364c6661b1625b6be2
barbiekk <69235339+barbiekk@users.noreply.github.com>
2020-10-16 10:43:16 +0700

    Update ListTypeMachine.php

--- TypeMachine/ListTypeMachine.php
+++ TypeMachine/ListTypeMachine.php
@@ -13,7 +13,7 @@
 
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-    <link href="../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="../vendor/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
     <link href="../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
     <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
