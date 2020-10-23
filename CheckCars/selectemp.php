aabcc2ce46745462e8a9025c118af464cdff8b0b
bagayado65 <58890509+bagayado65@users.noreply.github.com>
2020-10-24 01:43:13 +0700

    alllllllllllllllllllllllllllllllllllllll uuuhuuu

--- CheckCars/selectemp.php
+++ CheckCars/selectemp.php
@@ -1,25 +1,74 @@
+
 <!DOCTYPE html>
 <html>
-<head>
- <meta charset="utf-8">
- <title>Test</title>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
-<body>
- <select id="a">
-  <option name="One" value="1">One</option>
-  <option name="Two" value="2">Two</option>
-  <option name="Three" value="3">Three</option>
-  <option name="Four" value="4">Four</option>
- </select>
-<input type="text" name="" id="texts" value="">
-<input type="submit" name="submit" id="submit" value="ยันยัน">
-<script>
-  $(document).ready(function() {
-    $('#submit').click(function() {
-      $('#texts').val($('#a :selected').val());
-    });
-  });
-</script>
-</body>
+ <head>
+  <title>Webslesson Tutorial | How to Use Bootstrap Select Plugin with PHP JQuery</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+
+ </head>
+ <body>
+  <br /><br />
+  <div class="container">
+   <br />
+   <h2 align="center">How to Use Bootstrap Select Plugin with PHP JQuery</h2>
+   <br />
+   <div class="col-md-4" style="margin-left:200px;">
+    <form method="post" id="multiple_select_form">
+     <select name="framework" id="framework" class="form-control selectpicker" data-live-search="true" multiple>
+      <option value="Laravel">Laravel</option>
+      <option value="Symfony">Symfony</option>
+      <option value="Codeigniter">Codeigniter</option>
+      <option value="CakePHP">CakePHP</option>
+      <option value="Zend">Zend</option>
+      <option value="Yii">Yii</option>
+      <option value="Slim">Slim</option>
+     </select>
+     <br /><br />
+     <input type="hidden" name="hidden_framework" id="hidden_framework" />
+     <input type="submit" name="submit" class="btn btn-info" value="Submit" />
+    </form>
+    <br />
+
+   </div>
+  </div>
+ </body>
 </html>
+
+<script>
+$(document).ready(function(){
+ $('.selectpicker').selectpicker();
+
+ $('#framework').change(function(){
+  $('#hidden_framework').val($('#framework').val());
+ });
+
+ $('#multiple_select_form').on('submit', function(event){
+  event.preventDefault();
+  if($('#framework').val() != '')
+  {
+   var form_data = $(this).serialize();
+   $.ajax({
+    url:"insert.php",
+    method:"POST",
+    data:form_data,
+    success:function(data)
+    {
+     //console.log(data);
+     $('#hidden_framework').val('');
+     $('.selectpicker').selectpicker('val', '');
+     alert(data);
+    }
+   })
+  }
+  else
+  {
+   alert("Please select framework");
+   return false;
+  }
+ });
+});
+</script>
