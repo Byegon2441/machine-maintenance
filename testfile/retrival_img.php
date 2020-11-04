90183e7351d8ec496e34a53b17dd953acf9bc904
yukiharasaki <pongsaton.san@mail.pbru.ac.th>
2020-11-04 11:04:08 +0700

    img

--- testfile/retrival_img.php
+++ testfile/retrival_img.php
@@ -1,23 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    .column {
+        float: left;
+        width: 20%;
+        padding: 10px;
+    }
+
+    /* Style the images inside the grid */
+    .column img {
+        opacity: 0.8;
+        cursor: pointer;
+    }
+
+    .column img:hover {
+        opacity: 1;
+    }
+
+    .closebtn {
+        position: absolute;
+        right: 50px;
+        color: #FFFFFF;
+        font-size: 35px;
+        cursor: pointer;
+    }
+    </style>
 </head>
+
 <body>
-<?php
+    <?php
     include '../Template/templCRUD.php';?>
     <div class="container">
         <div class="panel panel-default" style="margin-top:20px;">
             <div class="panel-heading">
                 <h3 class="panel-title">รูปอะไหล่</h3>
             </div>
-                <div class="panel-body">
-                    <div class="col">
-                        <div class="col-md-12">
-            <?php
+            <div class="panel-body">
+                <div class="col">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <?php
             include '../database/connect.php';
             $ig = $_GET['id'];
             $igg = $_GET['seq'];
@@ -31,14 +60,32 @@
             $dirname = "../checkParts/$pat";
             $images = glob($dirname."*.{jpg,gif,png}",GLOB_BRACE);
             
+            
             foreach($images as $image) {
-                echo '<img style="width: 500px; height:500px;" src="'.$image.'" />&nbsp </n>';
+                echo '<div class="column">
+                    <img src="'.$image.'" style="width:100%" onclick="myFunction(this);">
+                </div>';
             }
             ?>
+                        </div>
+                        <div class="container">
+                            <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+                            <img id="expandedImg" style="width:92%">
+                            <div id="imgtext"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+    function myFunction(imgs) {
+        var expandImg = document.getElementById("expandedImg");
+        expandImg.src = imgs.src;
+        expandImg.parentElement.style.display = "block";
+    }
+    </script>
 </body>
+
 </html>
