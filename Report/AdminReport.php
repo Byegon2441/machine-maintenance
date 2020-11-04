3a6f25b02a5b97d9413dafbd1587012097aceb5a
yukiharasaki <pongsaton.san@mail.pbru.ac.th>
2020-11-04 11:51:56 +0700

    Update AdminReport.php

--- Report/AdminReport.php
+++ Report/AdminReport.php
@@ -19,6 +19,7 @@
 
     <!-- DataTables CSS -->
     <link href="../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="../vendor/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
 
     <!-- DataTables Responsive CSS -->
     <link href="../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
@@ -28,6 +29,7 @@
 
     <!-- Custom Fonts -->
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/css/datepicker.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -40,31 +42,18 @@
 
 <body>
 
-<?php include '../Template/templsidebar.php';?>
+    <?php include '../Template/templsidebar.php';?>
 
     <div id="wrapper">
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">รายงาน
-                    </h1>
-                        <div class="form-group col-md-2 center">
-                            <select name="month" id="month" class="form-control form-control-sm"> 
-                                <option value="มกราคม">มกราคม</option>
-                                <option value="กุมพาพันธ์">กุมพาพันธ์</option>
-                                <option value="มีนาคม">มีนาคม</option>
-                                <option value="เมษายน">เมษายน</option>
-                                <option value="พฤษภาคม">พฤษภาคม</option>
-                                <option value="มิถุนาคม">มิถุนาคม</option>
-                                <option value="กรกฎาคม">กรกฎาคม</option>
-                                <option value="สิงหาคม">สิงหาคม</option>
-                                <option value="กันยายน">กันยายน</option>
-                                <option value="ตุลาคม">ตุลาคม</option>
-                                <option value="พฤศจิกายน">พฤศจิกายน</option>
-                                <option value="ธันวาคม">ธันวาคม</option>
-                            </select>
-                        </div>
-                       
+                    <h1 class="page-header">รายงาน</h1>
+                    <div class="form-group col-md-2 center">
+                        <label for="name" class="control-label">เลือกวันที่:</label>
+                        <input type="text" class="form-control" data-toggle="datepicker">
+                    </div>
+
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -73,11 +62,12 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                                ใบแจ้งซ่อม
+                            สถานะแจ้งซ่อมปัจจุบัน
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
-                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <table width="100%" class="table table-striped table-bordered table-hover display"
+                                id="dataTables-example">
                                 <thead>
                                     <tr>
                                         <th>เลขที่ใบแจ้งซ่อม</th>
@@ -90,54 +80,182 @@
                                 </thead>
                                 <tbody>
                                     <tr class="odd gradeA">
-                                    <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXXXXXXXXX</td>
                                         <td>XXXXXXX</td>
                                         <td>XXX</td>
                                         <td>XXXXXX</td>
                                         <td>แจ้งซ่อม</td>
-                                  
-                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด'/></td>
-                                      
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
                                     </tr>
                                     <tr class="odd gradeA">
-                                    <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXXXXXXXXX</td>
                                         <td>XXXXXXX</td>
                                         <td>XXX</td>
                                         <td>XXXXXX</td>
                                         <td>แจ้งซ่อม</td>
-                                  
-                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด'/></td>
-                                      
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
                                     </tr>
                                     <tr class="odd gradeA">
-                                    <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXXXXXXXXX</td>
                                         <td>XXXXXXX</td>
                                         <td>XXX</td>
                                         <td>XXXXXX</td>
                                         <td>แจ้งซ่อม</td>
-                                  
-                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด'/></td>
-                                      
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
                                     </tr>
+                                </tbody>
+                            </table>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            ซ่อมเสร็จรายวัน
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover display"
+                                id="dataTables-example2">
+                                <thead>
+                                    <tr>
+                                        <th>เลขที่ใบแจ้งซ่อม</th>
+                                        <th>วันที่แจ้ง</th>
+                                        <th>หมายเลขเครื่องจักร</th>
+                                        <th>ชื่องานหรือชื่อเครื่องจักร</th>
+                                        <th>สถานะ</th>
+                                        <th>จัดการ</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     <tr class="odd gradeA">
-                                    <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXXXXXXXXX</td>
                                         <td>XXXXXXX</td>
                                         <td>XXX</td>
                                         <td>XXXXXX</td>
-                                        <td>XXXXXXX</td>
-                                  
-                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด'/></td>
-                                      
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
                                     </tr>
                                     <tr class="odd gradeA">
-                                    <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXXXXXXXXX</td>
                                         <td>XXXXXXX</td>
                                         <td>XXX</td>
                                         <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
                                         <td>XXXXXXX</td>
-                                  
-                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด'/></td>
-                                      
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            ปิดงานรายวัน
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover display"
+                                id="dataTables-example2">
+                                <thead>
+                                    <tr>
+                                        <th>เลขที่ใบแจ้งซ่อม</th>
+                                        <th>วันที่แจ้ง</th>
+                                        <th>หมายเลขเครื่องจักร</th>
+                                        <th>ชื่องานหรือชื่อเครื่องจักร</th>
+                                        <th>สถานะ</th>
+                                        <th>จัดการ</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="odd gradeA">
+                                        <td>XXXXXXXXXXXXXX</td>
+                                        <td>XXXXXXX</td>
+                                        <td>XXX</td>
+                                        <td>XXXXXX</td>
+                                        <td>แจ้งซ่อม</td>
+                                        <td><input class='btn btn-primary' type='button' value='รายละเอียด' /></td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -161,14 +279,31 @@
     <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
     <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
     <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+    <script src="../vendor/js/datepicker.js"></script>
+    <script src="../vendor/js/datepicker.th-TH.js"></script>
 
     <!-- Page-Level Demo Scripts - Tables - Use for reference -->
     <script>
     $(document).ready(function() {
-        $('#dataTables-example').DataTable({
+        $('table.display').DataTable({
+            "scrollY": "200px",
+            "scrollCollapse": true,
+            "paging": false,
             responsive: true
         });
     });
+
+    $(function() {
+        $('[data-toggle="datepicker"]').datepicker({
+            autoHide: true,
+            autoPick: true,
+            language: 'th-TH',
+            zIndex: 2048,
+            format: 'dd/mm/yyyy'
+        });
+    });
+
+    $('.my-select').selectpicker();
     </script>
 
 </body>
