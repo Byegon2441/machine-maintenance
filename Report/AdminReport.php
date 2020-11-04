6313761bd97b41c15ab5a4cfe81f13bd6fe7a5aa
TARS-dev <pluszadio1998@gmail.com>
2020-11-04 15:27:20 +0700

    f

--- Report/AdminReport.php
+++ Report/AdminReport.php
@@ -2,13 +2,11 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>ระบบซ่อมบำรุงเครื่องจักร : รายงาน</title>
 
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -38,16 +36,14 @@
                         <input type="text" class="form-control datepicker" id="datee" data-toggle="datepicker">
                     </div>
                 </div>
-                <!-- /.col-lg-12 -->
             </div>
-            <!-- /.row -->
+
             <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             สถานะแจ้งซ่อมปัจจุบัน
                         </div>
-                        <!-- /.panel-heading -->
                         <div class="panel-body">
                             <table width="100%" class="table table-striped table-bordered table-hover display"
                                 id="dataTables-example">
