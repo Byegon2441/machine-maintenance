c884eea937e85a90f2181c667d3cf2d57718b307
yukiharasaki <pongsaton.san@mail.pbru.ac.th>
2020-11-05 17:33:35 +0700

    Update AdminReport.php

--- Report/AdminReport.php
+++ Report/AdminReport.php
@@ -178,28 +178,50 @@
     });
     $('.datepicker').datepicker();
     $('#datee').change(function() {
-        $.ajax({
-            type: "GET",
-            url: "closed_job.php",
-            data: {
-                date_fi: $('#datee').val()
-            },
-            dataType: "JSON",
-            success: function(data) {
-                alert(data.Code)
-                $('table.display').DataTable({
-                    "scrollY": "200px",
-                    "scrollCollapse": true,
-                    // "processing": true,
-                    // "serverSide": true,
-                    "paging": false,
-                    responsive: true
-                });
-            },
-            error: function() {
-                $('#ct').html("Some problem fetching data.Please try again");
-            }
-        });
+        var dated = $('#datee').val()
+        $('#closedJob').DataTable({
+            "scrollY": "200px",
+            "scrollCollapse": true,
+            "paging": false,
+            "ajax": {
+                "url": "closed_job.php",
+                "type": "GET",
+                "contentType": "application/json",
+                "data": 
+                function(d) {
+                    d.date_fi = $('#datee').val()
+                    return d.data
+                }
+                },
+                "columns":[
+                    { "data": "XVMajDocNo" },
+                    { "data": "XVVehCode" },
+                    { "data": "XVDptName" },
+                    { "data": "XVMajStatus" }
+                ]
+        })
+        // =========================================
+        $('#perDay').DataTable({
+            "scrollY": "200px",
+            "scrollCollapse": true,
+            "paging": false,
+            "ajax": {
+                "url": "perDay.php",
+                "type": "GET",
+                "contentType": "application/json",
+                "data": 
+                function(d) {
+                    d.date_fi = $('#datee').val()
+                    return d.data
+                }
+                },
+                "columns":[
+                    { "data": "XVMajDocNo" },
+                    { "data": "XVVehCode" },
+                    { "data": "XVDptName" },
+                    { "data": "XVMajStatus" }
+                ]
+        })
     });
     
     </script>
