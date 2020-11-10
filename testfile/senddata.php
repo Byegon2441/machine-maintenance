a0fa2bd2d41b1addb05499087eea7ed3a95fa380
Patthapong Niamkerd <aeknicotine@gmail.com>
2020-11-10 13:05:17 +0700

    ทดสอบการส่งข้อมูลlogin

---
+++ testfile/senddata.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<!-- <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+<script src="../vendor/datatables-responsive/dataTables.responsive.js"></script> -->
+<script src="../vendor/jquery/jquery.js"></script>
+<script src="../vendor/jquery/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+            // $.ajax({
+            //     type: 'POST',
+            //     url: 'https://networklink.co.th/ConCenter_Service/api/login',
+            //     success: function (data) {
+            //         //console.log(data);
+            //         console.log("OK");
+            //     },
+            //     error: function(error) {
+            //         console.log('Error: ' , error);
+                    
+            //     }
+            // });
+                    $.ajax({
+                    'url':'https://networklink.co.th/ConCenter_Service/api/login',
+                    'method':'POST',
+                    'dataType': 'json',
+                    processData: false,
+                    'contentType': 'application/json',
+                    'data':JSON.stringify({
+                        "XVUsrCode": "TestPBRU",
+                        "XVUsrPwd" : "123456789"
+                    }),
+                    'success': function (data) {
+                    console.log(data);
+                    console.log("OK");
+                },
+                error: function(error) {
+                    console.log('Error: ' , error);
+                    
+                }
+
+});
+            
+});
+</script>
+
+</html>
