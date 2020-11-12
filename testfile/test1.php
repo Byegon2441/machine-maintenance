195b408a2f694db34349417a55346c16b9090147
Patthapong Niamkerd <aeknicotine@gmail.com>
2020-11-12 15:32:28 +0700

    ทดสอบสิทธิ์การเข้าถึง
    1.เอาXVPrgCodeมาเทียบกับ'P-03'เท่านั้น
    2.เอาXVMnuCodeมาเทียบกับรายการที่กำหนด

--- testfile/test1.php
+++ testfile/test1.php
@@ -1,10 +1,26 @@
 <?php
-session_start();
- echo   $_SESSION['XVEmpCode']."<br>";
- echo  $_SESSION['XVEmpName']."<br>";
- echo  $_SESSION['XVUsrCode']."<br>";
- echo  $_SESSION['XVUsrEmail']."<br>";
- echo  $_SESSION['XVUsrName']."<br>";
- echo $_SESSION['exp']."<br>";
 
+session_start();
+if($_SESSION['XVPrgCode']== 'P-03' && $_SESSION['XVMnuCode']=='M-000018'){
+
+    echo "Page 1"."<br>";
+    echo   $_SESSION['XVEmpCode']."<br>";
+    echo  $_SESSION['XVEmpName']."<br>";
+    echo  $_SESSION['XVUsrCode']."<br>";
+    echo  $_SESSION['XVUsrEmail']."<br>";
+    echo  $_SESSION['XVUsrName']."<br>";
+    echo  $_SESSION['XVMnuCode']."<br>";
+    echo  $_SESSION['XVPrgCode']."<br>";
+    echo $_SESSION['exp']."<br>";
+
+    echo "<a href='test2.php'>go to 2</a>";
+    echo " <a href='logout.php'>logout</a>";
+}else{
+    
+    echo "<script>
+    alert('Not Access');
+    window.location.href='senddata.php';
+    </script>
+    ";
+}
 ?>
