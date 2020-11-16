2eaded1c6e2b27e46ca6fd481bcd4a91b1154658
bagayado65 <58890509+bagayado65@users.noreply.github.com>
2020-11-16 11:37:00 +0700

    Update insertMachine.php

--- Machine/insertMachine.php
+++ Machine/insertMachine.php
@@ -26,58 +26,55 @@
     $XVVehTypeCode = $_POST['XVVehTypeName'];
     $sql = "INSERT INTO tmstvehicle(XVVehName,XVVehRegistration,XVVehNumber,XVVehMango,XVVehBrand,XVVehModel,XVVehChassisNumber,XVVehEngineNumber, XVVehTypeCode , XVDptCode)
      VALUES ('$XVVehName','$XVVehRegistration','$XVVehNumber','$XVVehMango','$XVVehBrand','$XVVehModel','$XVVehChassisNumber','$XVVehEngineNumber','$XVVehTypeCode','$XVDptCode');";
-    $stmt = $dbh->query($sql);
-    if($stmt){
-        // echo '<script>';
-        // echo "alert('ทำการเพิ่มเครื่องจักรเรียบร้อยแล้ว !!!');";
-        // echo "window.location='ListMachine.php';";
-        // echo '</script>';
+    try {
+      $stmt = $dbh->query($sql);
+      echo '<script>';
 
-        echo '<script>';
-       
-        echo "Swal.fire({
-                title: 'สำเร็จ!',
-                text: 'ทำการเพิ่มเครื่องจักรเรียบร้อยแล้ว !!!',
-                icon: 'success',
-                confirmButtonText: 'Back'
-              }).then(function() {
-                window.location = 'ListMachine.php';
-            });";
-        echo '</script>';
-      }else{
+      echo "Swal.fire({
+              title: 'สำเร็จ!',
+              text: 'ทำการเพิ่มเครื่องจักรเรียบร้อยแล้ว !!!',
+              icon: 'success',
+              confirmButtonText: 'Back'
+            }).then(function() {
+              window.location = 'ListMachine.php';
+          });";
+      echo '</script>';
+    } catch (PDOException $e) {
+      //echo $e->getMessage();
+      // $error_detail = $dbh->errorInfo();
+      //print_r($error_detail);
+      $error_detail = $e->getMessage();
+    //  print $error_detail;
+      $error_show="";
+      if(strpos($error_detail, "XVVehRegistration_UQ")){
+         $error_show="--->ทะเบียนรถ ($XVVehRegistration) มีการลงทะเบียนแล้ว";
+      }else if(strpos($error_detail, "XVVehNumber_UQ")){
+         $error_show="--->เบอร์รถ ($XVVehNumber) มีการลงทะเบียนแล้ว";
+      }else if(strpos($error_detail, "XVVehMango_UQ")){
+         $error_show="--->เลขทะเบียน Mango ($XVVehMango) มีการลงทะเบียนแล้ว";
+      }else if(strpos($error_detail, "XVVehChassisNumber_UQ")){
+         $error_show="--->หมายเลขคัทซี ($XVVehChassisNumber) มีการลงทะเบียนแล้ว";
+      }else if(strpos($error_detail, "XVVehChassisNumber_UQ")){
+         $error_show="--->หมายเลขเครื่อง ($XVVehEngineNumber) มีการลงทะเบียนแล้ว";
+      }
+    }
+    echo '<script>';
+    echo "Swal.fire({
+            title: 'เกิดข้อผิดพลาด!',
+            text: 'ไม่สามารถทำการเพิ่มข้อมูลเครื่องจักรได้เนื่องจาก $error_show',
+            icon: 'error',
+            confirmButtonText: 'Back'
+          }).then(function() {
+            window.location = 'ListMachine.php';
+        });";
+    echo '</script>';
 
-        $error_detail = mysqli_error($connect);
-        $error_show="";
-        if(strpos($error_detail, "XVVehRegistration_UQ")){
-          $error_show="ทะเบียนรถ ($XVVehRegistration) มีการลงทะเบียนแล้ว";
-        }else if(strpos($error_detail, "XVVehNumber_UQ")){
-          $error_show="เบอร์รถ ($XVVehNumber) มีการลงทะเบียนแล้ว";
-        }else if(strpos($error_detail, "XVVehMango_UQ")){
-          $error_show="เลขทะเบียน Mango ($XVVehMango) มีการลงทะเบียนแล้ว";
-        }else if(strpos($error_detail, "XVVehChassisNumber_UQ")){
-          $error_show="หมายเลขคัทซี ($XVVehChassisNumber) มีการลงทะเบียนแล้ว";
-        }else if(strpos($error_detail, "XVVehChassisNumber_UQ")){
-          $error_show="หมายเลขเครื่อง ($XVVehEngineNumber) มีการลงทะเบียนแล้ว";
-        }
 
         // echo '<script>';
         // echo "alert('ไม่สามารถทำการเพิ่มข้อมูลเครื่องจักรได้".'\n'.$error_show."');";
         // //echo "alert('$error_show');";
         // echo "window.location='ListMachine.php';";
         // echo '</script>';
-
-        echo '<script>';
-        echo "Swal.fire({
-                title: 'เกิดข้อผิดพลาด!',
-                text: 'ไม่สามารถทำการเพิ่มข้อมูลเครื่องจักรได้',
-                icon: 'error',
-                confirmButtonText: 'Back'
-              }).then(function() {
-                window.location = 'ListMachine.php';
-            });";
-        echo '</script>';
-
-      }
     $dbh = null;
 
 ?>
