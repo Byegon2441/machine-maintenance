57abaf2df1cb7249c2bf0ae80322e85530f40640
barbiekk <69235339+barbiekk@users.noreply.github.com>
2020-11-20 22:13:09 +0700

    Update addFixDate.php

--- AllowMainte/addFixDate.php
+++ AllowMainte/addFixDate.php
@@ -122,6 +122,16 @@
                             <label>ใบแจ้งซ่อม</label>
                             <form action="insertAllowFixDate.php" method='POST' class="form-inline">
 
+                            <div class="row">
+                                    <div class="col-md-6"></div>
+                                    <div class="col-md-6 ml-auto">
+                                        <div class="col text-right">
+                                            <label for="numb">สถานะใบแจ้งซ่อม : <input type="text" name="XVMajDocstatus"
+                                                    id="jobstatus" class="form-control"
+                                                    value="<?php echo $row1["XVMajStatus"];?>" readonly></label>
+                                        </div>
+                                    </div>
+                                </div>
 
                                 <div class="row">
                                     <div class="col-md-6"></div>
@@ -483,6 +493,7 @@ $dbh = NULL;
 
         </div>
         <!-- /#wrapper -->
+        </div>
         <?php
          
         }
